3b5c6067a296957bed90f7e71612ec3b75745372
Mark A Guza <guzamaguza@gmail.com>
2020-02-23 01:57:08 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -61,7 +61,6 @@ class Game
   end
 
   def turn
-    puts "Hi"
     @board.display #display the board
     attempted_move = self.current_player.move(@board) #assign the attempted move to the current player
 
